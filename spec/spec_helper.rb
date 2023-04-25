a662c4c958be0621a1bf75c28f6a3291b5f370ee
gendosu <gendosu@gmail.com>
2023-04-25 09:03:13 +0000

    upload-artifact to github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require "rspec/collection_matchers"
 require File.join(File.dirname(__FILE__), "..", "lib", "html2text")
 
 require 'simplecov'
-SimpleCov.start 'rails' do
+SimpleCov.start do
   # 測定対象外ファイル・フォルダを記載する。
-  add_filter ['spec/',]
+  add_filter ['spec/lib',]
 end
