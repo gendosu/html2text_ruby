23a10ee4aadbf023538cd370bc2af31c0565318d
gendosu <gendosu@gmail.com>
2023-04-25 08:56:19 +0000

    add simplecov

--- html2text.gemspec
+++ html2text.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "colorize"
   s.add_development_dependency "rake"
   s.add_development_dependency "bundler-audit"
+  s.add_development_dependency "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ require "rspec"
 require "rspec/collection_matchers"
 
 require File.join(File.dirname(__FILE__), "..", "lib", "html2text")
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  # 測定対象外ファイル・フォルダを記載する。
+  add_filter ['spec/',]
+end
